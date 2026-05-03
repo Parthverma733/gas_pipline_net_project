cd66203fa1c62e29af7daa003540c812256f8e91
Parthverma733 <parthverma733@gmail.com>
2026-05-03 12:29:13 +0530

    Update run.bat

--- gas_pipline_net_project/run.bat
+++ gas_pipline_net_project/run.bat
@@ -1,4 +1,4 @@
 @echo off
 
-start cmd /k "cd frontend && npm run dev"
-start cmd /k "cd backend && uvicorn main:app --reload"
+start cmd /k "cd frontend && npm i && npm i axios && npm run dev"
+start cmd /k "cd backend && pip install -r requirnments.txt && uvicorn main:app --reload"
